7a48ac8078d1a758d77d2fa260e122dd7841ff92
Ernesto-Sanchez-J <56661723+Ernesto-Sanchez-J@users.noreply.github.com>
2020-06-21 12:25:51 -0500

    added maaterial data table

--- MaterialTable/src/app/table/table.component.ts
+++ MaterialTable/src/app/table/table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IClassMember } from '../Interface/class-member';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-table',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+
+  displayedColumns: string[] = ['Seat', 'Name', 'Grade', 'Passing'];
+
 
   constructor() { }
 
