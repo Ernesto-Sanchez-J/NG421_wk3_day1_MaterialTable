177d1e7fdeb31923e6df039416abf712df89ea45
Ernesto-Sanchez-J <56661723+Ernesto-Sanchez-J@users.noreply.github.com>
2020-06-20 21:14:24 -0500

    finished data on class member

--- MaterialTable/src/app/Services/data.service.ts
+++ MaterialTable/src/app/Services/data.service.ts
@@ -1,12 +1,67 @@
 import { Injectable } from '@angular/core';
 import { ClassMember} from '../Interface/class-member';
+import { TmplAstRecursiveVisitor } from '@angular/compiler';
 
 const ClassMember: ClassMember[] = [
   {
     Seat: 1,
-    Name: 'Rob Luci'
+    Name: 'Rob Luci',
     Grade: 90,
     Passing: true
+  },
+  {
+    Seat: 2,
+    Name: 'Nami',
+    Grade: 98,
+    Passing: true
+  },
+  {
+    Seat: 3,
+    Name: 'Ussop',
+    Grade: 60,
+    Passing: false
+  },
+  {
+    Seat: 4,
+    Name: 'Sanji',
+    Grade: 78,
+    Passing: true
+  },
+  {
+    Seat: 5,
+    Name: 'Zoro',
+    Grade: 67,
+    Passing: false
+  },
+  {
+    Seat: 6,
+    Name: 'Chopper',
+    Grade: 100,
+    Passing: true
+  },
+  {
+    Seat: 7,
+    Name: 'Franky',
+    Grade: 94,
+    Passing: true
+  },
+  {
+    Seat: 8,
+    Name: 'Brook',
+    Grade: 55,
+    Passing: false
+  },
+  {
+    Seat: 9,
+    Name: 'Nico Robin',
+    Grade: 88,
+    Passing: true
+  },
+  {
+    Seat: 10,
+    Name: 'Luffy',
+    Grade: 49,
+    Passing: false
   },
 
 ];
